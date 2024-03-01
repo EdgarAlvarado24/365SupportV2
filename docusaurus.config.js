aaac2145f033bc7788943b2fe4c9537e66a6f134
EdgarAlvarado24 <edgar.alvarado.16@gmail.com>
2024-03-01 18:54:24 +0000

    copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -134,7 +134,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} 365monitoreo.com`,
       },
       prism: {
         theme: lightCodeTheme,
